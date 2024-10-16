2ef8a738d241c63e96f8fee2f7e67d96e0c8a1d0
wajid ansari <112863100+ansariwajid9999@users.noreply.github.com>
2024-10-16 10:56:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your code here
+const prices = document.querySelectorAll(".prices")
+let grandTotal = 0;
+
+prices.forEach((price) => {
+    grandTotal += Number(price.innerHTML)
+})
+
+const newRow = document.createElement("tr")
+
+const attribute = document.createElement("td")
+attribute.textContent = "grandTotal"
+attribute.classList = ".attributes"
+
+const element = document.createElement("td")
+element.textContent = grandTotal
+element.classList = ".prices"
+
+newRow.appendChild(attribute)
+newRow.appendChild(element)
+
+const table = document.querySelector(".tableClass");
+table.appendChild(newRow);
